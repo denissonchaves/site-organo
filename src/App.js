50d07f675f17dfdb5cd5b186a455ad77a8450600
denissonchaves <denisson.chaves@hotmail.com>
2022-08-31 16:39:57 -0300

    refactor: remove 'const times' de Formulario e a eleva para App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner/Banner';
 import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
 
 function App() {
+
+  const times = [
+    {
+      nome: 'Programação',
+      corPrimaria: '#57c278',
+      corSecundaria: '#d9f7e9'
+    },
+    {
+      nome: 'Front-end',
+      corPrimaria: '#82cffa',
+      corSecundaria: '#e8f8ff'
+    },
+    {
+      nome: 'Data Science',
+      corPrimaria: '#a6d157',
+      corSecundaria: '#f0f8e2'
+    },
+    {
+      nome: 'Devops',
+      corPrimaria: '#e06b69',
+      corSecundaria: '#fde7e8'
+    },
+    {
+      nome: 'UX e Design',
+      corPrimaria: '#db6ebf',
+      corSecundaria: '#fae9f5'
+    },
+    {
+      nome: 'Mobile',
+      corPrimaria: '#ffba05',
+      corSecundaria: '#fff5d9'
+    },
+    {
+      nome: 'Inovação e Gestão',
+      corPrimaria: '#ff8a29',
+      corSecundaria: '#ffeedf'
+    }
+  ];
 
   const [colaboradores, setColaboradores] = useState([]);
 
@@ -15,12 +54,16 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario
+        times={times.map(time => time.nome)}
         aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}
         
         // aoColaboradorCadastrado={function (colaborador) {
         //   return aoNovoColaboradorAdicionado(colaborador);
         // }}
       />
+      
+      {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria} />)}
+
     </div>
   );
 }
